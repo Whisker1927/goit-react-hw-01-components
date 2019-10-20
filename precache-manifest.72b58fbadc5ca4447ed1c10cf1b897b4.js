746b627b2ce73bc75faad766d42b9c6e57d3e4a9
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-10-21 02:01:52 +0300

    Updates

---
+++ precache-manifest.72b58fbadc5ca4447ed1c10cf1b897b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d7cd0514a3b861283a6b566f0107bd",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "729d902d057236239ec1",
+    "url": "/goit-react-hw-01-components/static/css/main.24b591ed.chunk.css"
+  },
+  {
+    "revision": "fb6a84cc7eb32ae6d325",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf34202.chunk.js"
+  },
+  {
+    "revision": "729d902d057236239ec1",
+    "url": "/goit-react-hw-01-components/static/js/main.a050234f.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
